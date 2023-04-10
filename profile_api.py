6067ee5205ea0f2a5cb2deedce4f6d713149aadd
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-04-10 12:43:22 +0000

    create user

--- profile_api.py
+++ profile_api.py
@@ -17,7 +17,23 @@ def get_user_byID(user_id):
 
     if user.exists:
         user_dict = user.to_dict()
-        user_dict['user_id'] = user.id
+        user_dict['id'] = user.id
         return jsonify(user_dict), 200
     return jsonify({'message': 'User ID not found'}), 404
 
+# @app.route('/users', methods=['POST'])
+def create_user():
+    record = json.loads(request.data)
+
+    users_data = db.collection('users')
+    query_check = users_data.where('id', '==', record['id'])
+    users = query_check.stream()
+
+    for user in users:
+        return jsonify({'message':f'User with id {record["id"]} already exists.'}), 400
+    
+    user_data = users_data.document()
+    user_data.set(record)
+    record['id'] = user_data.id
+
+    return jsonify(record), 201
