7364224b97699748df052715d129bdec6437151c
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-04-19 10:43:13 +0000

    signup page ui

--- my_ashx_app/lib/screens/signup.dart
+++ my_ashx_app/lib/screens/signup.dart
@@ -1,142 +1,107 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 
 class StudentForm extends StatefulWidget {
+  const StudentForm({Key? key}) : super(key : key);
+
   @override
   _StudentFormState createState() => _StudentFormState();
 }
 
-class _StudentFormState extends State<StudentForm> {
-  final _formKey = GlobalKey<FormState>();
-  late String _studentId;
-  late String _name;
-  late String _email;
-  late DateTime _dob;
-  late int _yearGroup;
-  late String _major;
-  late bool _hasResidence = false;
-  late String _bestFood;
-  late String _bestMovie;
+class _StudentFormState extends State<StudentForm> 
+{
+  // final _formKey = GlobalKey<FormState>();
+  String studentId = "";
+  String name = "";
+  String email = "";
+  DateTime dob = DateTime.now();
+  int yearGroup = 0;
+  String major = "";
+  bool hasResidence = false;
+  String bestFood = "";
+  String bestMovie = "";
 
-  void _submitForm() {
-    if (_formKey.currentState?.validate() ?? false) {
-      // Submit the form
-      // You can handle the form submission logic here
-      print('Student ID: $_studentId');
-      print('Name: $_name');
-      print('Email: $_email');
-      print('Date of Birth: $_dob');
-      print('Year Group: $_yearGroup');
-      print('Major: $_major');
-      print('Has Residence: $_hasResidence');
-      print('Best Food: $_bestFood');
-      print('Best Movie: $_bestMovie');
-    }
-  }
+  // void _submitForm() {
+  //   if (_formKey.currentState?.validate() ?? false) {
+  //     // Submit the form
+  //     // You can handle the form submission logic here
+  //     print('Student ID: $_studentId');
+  //     print('Name: $_name');
+  //     print('Email: $_email');
+  //     print('Date of Birth: $_dob');
+  //     print('Year Group: $_yearGroup');
+  //     print('Major: $_major');
+  //     print('Has Residence: $_hasResidence');
+  //     print('Best Food: $_bestFood');
+  //     print('Best Movie: $_bestMovie');
+  //   }
 
   @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-        TextFormField(
-          decoration: InputDecoration(labelText: 'Student ID'),
-          validator: (value) {
-            if (value?.isEmpty ?? true) {
-              return 'Please enter your student ID';
-            }
-            return null;
-          },
-          onSaved: (value) => _studentId = value ?? '',
-        ),
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * .5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  // logo
+                  Image.asset(
+                    "images/black-student-png.jpg"
+                  ),
 
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Name'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your name';
-              }
-              return null;
-            },
-            onSaved: (value) => _name = value ?? '',
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your email address';
-              }
-              return null;
-            },
-            onSaved: (value) => _email = value ?? '',
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Date of Birth'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your date of birth';
-              }
-              return null;
-            },
-            onSaved: (value) => _dob = DateTime.parse(value ?? ''),
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Year Group'),
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your year group';
-              }
-              return null;
-            },
-            onSaved: (value) => _yearGroup = int.parse(value ?? ''),
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Major'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your major';
-              }
-              return null;
-            },
-            onSaved: (value) => _major = value ?? '',
-          ),
-          CheckboxListTile(
-            title: Text('Do you have on-campus residence?'),
-            value: _hasResidence, // add a null check here
-            onChanged: (bool? value) {
-              setState(() {
-                _hasResidence = value!;
-              });
-            },
-          ),
+                  const SizedBox(height: 20),
+                  // studentId text field
+                  TextField(
+                    onChanged: (value){
+                      studentId = value;
+                    },
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 2)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 2)),
+                      hintText: "Student ID",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      icon: Icon(Icons.person, color:Colors.black,)
+                    )
+                  ),
 
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Best Food'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your best food';
-              }
-              return null;
-            },
-            onSaved: (value) => _bestFood = value ?? '',
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Best Movie'),
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return 'Please enter your best movie';
-              }
-              return null;
-            },
-            onSaved: (value) => _bestMovie = value ?? '',
-          ),
-          ElevatedButton(
-            onPressed: _submitForm,
-            child: Text('Submit'),
+                  const SizedBox(height: 10),
+                  // name text field
+                  TextField(
+                    onChanged: (value){
+                      name = value;
+                    },
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 2)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black, width: 2)),
+                      hintText: "Name",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      icon: Icon(Icons.person_4, color:Colors.black,)
+                    )
+                  ),
+
+                  const SizedBox(height: 20),
+                  // button
+                  ElevatedButton(
+                    onPressed: (){
+
+                    },
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 100, vertical: 20)),
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    child: const Text("Sign Up",
+                    style: TextStyle(color: Colors.black, letterSpacing: 2, fontSize: 15,))
+                  )
+                  
+                ]
+              )
+            ),
           ),
         ],
       ),
